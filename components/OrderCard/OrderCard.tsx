1d9c16110c4c9716c46bdff9e5ed3ee95c064d2b
Chris Ruzin <457202+nizur@users.noreply.github.com>
2022-10-09 22:59:07 -0500

    Minor changes:
    - Add cancel button to OrderCard component
    - Change dark theme background color again
    - Bring in React Query to make building the Orders page easier

--- components/OrderCard/OrderCard.tsx
+++ components/OrderCard/OrderCard.tsx
@@ -1,15 +1,17 @@
 import Image from 'next/image';
-import type { Order } from '../../types/pizza';
+import type { PlacedOrder } from '../../types/pizza';
+import cancelIcon from '../../public/x.svg';
 
 interface OrderCardProps {
-  order: Order;
+  onCancel?: (id: string) => Promise<void>;
+  order: PlacedOrder;
 }
 
-function OrderCard({ order }: OrderCardProps) {
+function OrderCard({ onCancel, order }: OrderCardProps) {
   const flavorImg = order.Flavor.toLowerCase().replaceAll(' ', '_');
 
   return (
-    <div className="max-w-[280px] rounded overflow-hidden shadow-lg bg-inherit border">
+    <div className="relative max-w-[280px] rounded overflow-hidden shadow-lg bg-inherit border">
       <Image
         className="w-full aspect-video"
         src={`/${flavorImg}.jpg`}
@@ -18,14 +20,25 @@ function OrderCard({ order }: OrderCardProps) {
         height="70px"
         width="100%"
       />
-      <div className="px-6 py-4">
+      <div className="px-6 py-4 min-h-[180px]">
         <div className="font-bold text-xl mb-2 capitalize">{order.Size} {order.Flavor}</div>
         <p className="text-base">
           {order.Size} {order.Flavor} on {order.Crust} crust
         </p>
       </div>
-      <div className="px-6 pt-4 pb-2">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Table #{order.Table_No}</span>
+      <div className="absolute bottom-0 left-0 w-full px-6 pt-4 pb-2 flex justify-between items-center">
+        <div className="flex-auto text-sm font-semibold">Table #{order.Table_No}</div>
+        <span 
+          className="group flex items-center gap-2 flex-none text-red-600 text-sm font-semibold cursor-pointer"
+          onClick={() => onCancel(order.Order_ID)}>
+          <span className="text-transparent group-hover:text-red-600 transition-all ease-in-out">Cancel</span>
+          <img
+            {...cancelIcon}
+            width="12"
+            height="12"
+            alt="Cancel order"
+          />
+        </span>
       </div>
     </div>
   );
